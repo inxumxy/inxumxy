de7916395377b1f1285e636800432f656fbb671b
Alisher <inxumxy@gmail.com>
2024-09-23 21:28:19 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,26 @@
-const form = document.querySelector('form');
-const latitudeInput = document.querySelector('#latitude');
-const longitudeInput = document.querySelector('#longitude');
-const timezoneInput = document.querySelector('#timezone');
-const autoLocateButton = document.querySelector('#auto-locate');
-const prayerTimesSection = document.querySelector('#prayer-times');
+const prayerTimesElement = document.querySelector('.prayer-times');
+const detectLocationButton = document.querySelector('#detect-location');
+const timezoneSelect = document.querySelector('#timezone');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const latitude = latitudeInput.value;
-    const longitude = longitudeInput.value;
-    const timezone = timezoneInput.value;
-    const prayTimes = new PrayTimes('ISNA');
-    const times = prayTimes.getTimes(new Date(), [latitude, longitude], timezone);
-    displayPrayerTimes(times);
-});
+// Aladhan API endpoint
+const apiEndpoint = 'https://api.aladhan.com/v1/timings';
 
-autoLocateButton.addEventListener('click', () => {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-            const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-            latitudeInput.value = latitude;
-            longitudeInput.value = longitude;
-            timezoneInput.value = timezone;
-            form.dispatchEvent(new Event('submit'));
-        }, error => {
-            console.error(error);
-        });
-    } else {
-        alert('Geolocation is not supported by this browser.');
-    }
-});
-
-function displayPrayerTimes(times) {
-    const fajrTime = document.querySelector('#fajr');
-    const dhuhrTime = document.querySelector('#dhuhr');
-    const asrTime = document.querySelector('#asr');
-    const maghribTime = document.querySelector('#maghrib');
-    const ishaTime = document.querySelector('#isha');
-
-    fajrTime.textContent = times.fajr;
-    dhuhrTime.textContent = times.dhuhr;
-    asrTime.textContent = times.asr;
-    maghribTime.textContent = times.maghrib;
-    ishaTime.textContent = times.isha;
+// Function to get prayer times
+async function getPrayerTimes(latitude, longitude, timezone) {
+    const response = await fetch(`${apiEndpoint}/${latitude}/${longitude}/${timezone}`);
+    const data = await response.json();
+    return data.data.timings;
 }
+
+// Function to display prayer times
+function displayPrayerTimes(prayerTimes) {
+    prayerTimesElement.innerHTML = '';
+    Object.keys(prayerTimes).forEach((key) => {
+        const time = prayerTimes[key];
+        const element = document.createElement('h2');
+        element.textContent = `${key}: ${time}`;
+        prayerTimesElement.appendChild(element);
+    });
+}
+
+// Function
