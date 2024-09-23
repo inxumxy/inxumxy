eb1284a9ef89edffcd89f81a4aa9006c459d6807
Alisher <inxumxy@gmail.com>
2024-09-23 20:52:47 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const form = document.querySelector('form');
 const latitudeInput = document.querySelector('#latitude');
 const longitudeInput = document.querySelector('#longitude');
 const timezoneInput = document.querySelector('#timezone');
+const autoLocateButton = document.querySelector('#auto-locate');
 const prayerTimesSection = document.querySelector('#prayer-times');
 
 form.addEventListener('submit', (e) => {
@@ -14,9 +15,34 @@ form.addEventListener('submit', (e) => {
     displayPrayerTimes(times);
 });
 
+autoLocateButton.addEventListener('click', () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+            latitudeInput.value = latitude;
+            longitudeInput.value = longitude;
+            timezoneInput.value = timezone;
+            form.dispatchEvent(new Event('submit'));
+        }, error => {
+            console.error(error);
+        });
+    } else {
+        alert('Geolocation is not supported by this browser.');
+    }
+});
+
 function displayPrayerTimes(times) {
     const fajrTime = document.querySelector('#fajr');
     const dhuhrTime = document.querySelector('#dhuhr');
     const asrTime = document.querySelector('#asr');
     const maghribTime = document.querySelector('#maghrib');
     const ishaTime = document.querySelector('#isha');
+
+    fajrTime.textContent = times.fajr;
+    dhuhrTime.textContent = times.dhuhr;
+    asrTime.textContent = times.asr;
+    maghribTime.textContent = times.maghrib;
+    ishaTime.textContent = times.isha;
+}
